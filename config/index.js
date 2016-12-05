b5de2ff8ed05ca90fe77c0f259a9e4da21aa4547
Jay Zawrotny <jayzawrotny@gmail.com>
2016-12-05 17:00:16 -0500

    Uhhh I think I accidentally finished the first draft of this app

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 let env = process.env.NODE_ENV || 'development';
-let config = require('./config/shared.json');
+let config = require('./shared.json');
 
 module.exports = Object.assign(
   {},
   config,
-  require(`./config/${env}.json`) || {}
+  require(`./${env}.json`) || {}
 );
--- routes/v1/github.js
+++ routes/v1/github.js
@@ -1,50 +1,20 @@
+let _ = require('highland');
+let debug = require('debug')('webhooks:routes')
 let express = require('express');
-let request = require('request');
 let router = express.Router();
+let util = require('../../lib/util/toJSON')
+let GithubWebhook = require('../../lib/webhooks/GithubWebhook');
+let githubWebhook = new GithubWebhook();
 
-const githooks = [];
-const config = require('../config');
+const config = require('../../config');
+const url = `/${config.keys.github_webhook}`;
 
-function logRequest (request) {
-  if (githooks.length > 20) githooks.pop();
+debug(`Listening for posts to /v1/github/${url}`)
 
-  githooks.unshift(request.body);
-}
+module.exports = router.post(url, (req, res, next) => {
+  githubWebhook
+    .write(req.body)
+    .map(toJSON({ status: 'ok' }))
+    .pipe(res);
+});
 
-function postToSlackTech (req, res, next) {
-  return new Promise((resolve) => {
-    request({
-      uri: config.slack.hook_url,
-      method: 'POST',
-      json: {
-      }
-    });
-  });
-}
-
-function postToSlackGit (req, res, next) {
-  return new Promise((resolve) => {
-
-  });
-}
-
-function forwardToClubhouse (req, res, next) {
-  return new Promise((resolve) => {
-
-  });
-}
-
-module.exports = router
-  .post(`/${config.keys.github_webhook}`, function githubWebhookHandler (req, res, next) {
-    Promise.all([
-      postToSlackTech(req, res, next),
-      postToSlackGit(req, res, next),
-      forwardToClubhouse(req, res, next),
-    ])
-    .then(() => {
-      res.send({ status: 'ok' });
-    })
-    .catch((err) => {
-      next(err);
-    });
-  });
